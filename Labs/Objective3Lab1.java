1c11b8901dbc93ca0631382b731a13b7c0f9101c
Brian <garyramirez007@gmail.com>
2021-04-26 14:24:23 -0600

    complete Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,11 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+
+    int num1 = 17;
+    int num2 = 3;
+    int num3 = 6;
+    double average = 8.6666666666667;
+
+System.out.println(average);
+  }
+}
