a461132f6b10815e481b0c9799a5e8b5468624f4
Brian <garyramirez007@gmail.com>
2021-04-26 01:13:08 -0600

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Brian Ramirez";
+    int age = 29;
+    String state = "Colorado";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
