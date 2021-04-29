80059eb53a4a85425edd6f35dbf8c2f0f274d697
Brian <garyramirez007@gmail.com>
2021-04-29 00:50:29 -0600

    complete Objective7Lab2.java

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,11 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+
+  int counter = 0;
+
+  do {
+    System.out.println(counter);
+    counter = counter + 1;
+  } while(counter <= 20);
+  }
+}
